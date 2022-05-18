e52d9b2c7878385b07c21663ee849b2fc3af92e4
HieuNguyen <hieu14101998@gmail.com>
2022-05-18 20:20:08 +0700

    delete import button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import { useState } from 'react';
 import {Navbar, NavbarBrand, Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Button } from 'reactstrap';
 import { STAFFS } from './shared/staffs';
 import ListUser from '../src/components/StaffListComponent'
 import './App.css';
