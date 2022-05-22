f5e5fd84a6e8cfed75b8fbe352d3d81a4238d635
HieuNguyen <hieu14101998@gmail.com>
2022-05-22 10:56:01 +0700

    commit 10

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import React from "react";
 import { useState } from "react";
 import {
   Navbar,
@@ -13,17 +13,25 @@ import { STAFFS } from "./shared/staffs";
 import ListUser from "../src/components/StaffListComponent";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs:STAFFS
+    }
+  }
+  render(){
+    return (
+      <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="#">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
         </div>
       </Navbar>
-      <ListUser />
+      <ListUser staffs={this.state.staffs}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,55 +1,65 @@
 import { STAFFS } from "../shared/staffs";
 import React, { Component } from "react";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardBody, CardTitle } from "reactstrap";
 import { useState } from "react";
 import dateFormat from "dateformat";
 
-const ListUser = function () {
-  const [user, setUser] = useState();
-
-  const handleClick = (index) => {
-    const result = STAFFS[index];
-    console.log(result);
-    setUser(() => (
+class ListUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectUser: null,
+    };
+  }
+  handleClick(value) {
+    this.setState({
+      selectUser: value,
+    });
+  }
+  renderUser(staffs) {
+    if (staffs != null) {
+      return (
       <div className="col-lg-6 col-md-6 col-12">
         <Card>
           <CardBody className="cinfor">
-            <h4>{"Họ và tên: " + result.name}</h4>
-            <p>{`Ngày sinh: ${dateFormat(result.doB, "dd/mm/yyyy")}`}</p>
+            <h4>{"Họ và tên: " + staffs.name}</h4>
+            <p>{`Ngày sinh: ${dateFormat(staffs.doB, "dd/mm/yyyy")}`}</p>
             <p>{`Ngày vào công ty: ${dateFormat(
-              result.startDate,
+              staffs.startDate,
               "dd/mm/yyyy"
             )}`}</p>
-            <p>{`Phòng ban: ${result.department.name}`}</p>
-            <p>{`Số ngày nghỉ còn lại: ${result.annualLeave}`}</p>
-            <p>{`Số ngày đã làm thêm: ${result.overTime}`}</p>
+            <p>{`Phòng ban: ${staffs.department.name}`}</p>
+            <p>{`Số ngày nghỉ còn lại: ${staffs.annualLeave}`}</p>
+            <p>{`Số ngày đã làm thêm: ${staffs.overTime}`}</p>
+          </CardBody>
+        </Card>
+      </div>
+      )
+    }
+    else{
+      return <div></div>
+    }
+  }
+
+  render() {
+    let menu = this.props.staffs.map((value) => (
+      <div className="col-lg-4 col-md-6 col-12 list" key={value.id}>
+        <Card onClick={() => this.handleClick(value)}>
+          <CardBody className="cbody">
+            <CardTitle>{value.name}</CardTitle>
           </CardBody>
         </Card>
       </div>
     ));
-  };
-
-  const Staffs = STAFFS.map((value, index) => (
-    <div className="col-lg-4 col-md-6 col-12 list" key={index}>
-      <Card onClick={() => handleClick(index)}>
-        <CardBody className="cbody">
-          <CardTitle>{value.name}</CardTitle>
-        </CardBody>
-      </Card>
-    </div>
-  ));
-  return (
-    <div className="container contain" style={{ padding: 15 }}>
-      <div className="row">{Staffs}</div>
-      <div className="row">
-        {user || "Vui lòng bấm vào tên để xem thông tin"}
+    return (
+      <div className="container contain">
+        <div className="row">{menu}</div>
+        <div className="row">
+          {this.renderUser(this.state.selectUser)}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ListUser;
